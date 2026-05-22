54c4454686ab1566019e09e0be5d3c40f878e15b
jsnape183 <jsnape183@gmail.com>
2026-05-22 13:12:18 +0100

    refactor: remove redundant localStorage writes from AssetTree, trust redux-persist (P3)

--- src/components/TreePanel/AssetTree/index.tsx
+++ src/components/TreePanel/AssetTree/index.tsx
@@ -2,8 +2,6 @@ import { useDispatch } from 'react-redux';
 import { useAssetsForProject } from '../../../hooks/useAssetsForProject';
 import FileInput, { FileUploadResult } from '../FileInput';
 import { addAsset } from '../../../features/assets/assetsSlice';
-import { useLocalStorage } from '../../../hooks/useLocalStorage';
-import { Asset } from '../../../globalProps/Asset';
 
 type AssetTreeProps = {
   projectId: string;
@@ -12,10 +10,6 @@ type AssetTreeProps = {
 const AssetTree: React.FC<AssetTreeProps> = ({ projectId }) => {
   const dispatch = useDispatch();
   const assets = useAssetsForProject(projectId);
-  const [storedAssets, setStoredAssets] = useLocalStorage<Asset[]>(
-    `${projectId}:assets`,
-    []
-  );
 
   const handleFileInputChange = (files: FileUploadResult[]) => {
     files.forEach((file) => {
@@ -24,11 +18,9 @@ const AssetTree: React.FC<AssetTreeProps> = ({ projectId }) => {
           id: crypto.randomUUID(),
           name: file.name,
           content: file.content,
-          projectId: projectId,
+          projectId,
         })
       );
-
-      setStoredAssets([...storedAssets, file as Asset]);
     });
   };
 
@@ -38,11 +30,9 @@ const AssetTree: React.FC<AssetTreeProps> = ({ projectId }) => {
       <FileInput onChange={handleFileInputChange} />
       <ul className="space-y-2 text-sm">
         {assets.map((asset) => (
-          <>
-            <li key={asset.id} className="hover:text-white cursor-pointer">
-              {asset.name}
-            </li>
-          </>
+          <li key={asset.id} className="hover:text-white cursor-pointer">
+            {asset.name}
+          </li>
         ))}
       </ul>
     </>
