0d96f8041de61ff4112f46fccae46d194918c998
jsnape183 <jsnape183@gmail.com>
2026-05-23 20:30:02 +0100

    feat: useFilesForProject returns files in fileOrder sequence

--- src/hooks/useFilesForProject.ts
+++ src/hooks/useFilesForProject.ts
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
-import { IFile } from "../features/files/filesSlice";
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { IFile } from '../features/files/filesSlice';
 
 export const useFilesForProject = (projectId: string): IFile[] => {
-  return useSelector((state: RootState) =>
-    Object.values(state.files.byId).filter(
-      (file) => (file as IFile).projectId === projectId
-    )
-  ) as Array<IFile>;
+  return useSelector((state: RootState) => {
+    const order = state.files.fileOrder[projectId];
+    if (!order || order.length === 0) {
+      // Fallback for persisted state that predates fileOrder
+      return Object.values(state.files.byId).filter(
+        (file) => file.projectId === projectId
+      ) as IFile[];
+    }
+    return order
+      .map((id) => state.files.byId[id])
+      .filter((file): file is IFile => Boolean(file));
+  });
 };
